ab1caa4b5b504c31009a2cdcad6c7db85d392ac2
Mark Ward <markmaw@gmail.com>
2015-05-10 02:10:16 -0400

    find nearest centroid

--- seq-kmeans.c
+++ seq-kmeans.c
@@ -3,7 +3,11 @@
 #include <math.h>
 #include "helper.h"
 
-double l2_distance(double *x1, double *x2, int d);
+#define SQR(x) (x)*(x)
+
+double l2_distance(double *x1, double *x2, options opt);
+void find_nearest_centroid(double *x, double **centroids, options opt, \
+                            int *idx, double *distance);
 
 int main(int argc, char **argv) {
 
@@ -18,18 +22,21 @@ int main(int argc, char **argv) {
     }
 
     read_data(data, opt);
-    for(i=0; i<opt.n_points; i++){
-        for(j=0; j<opt.dimensions; j++){
-            printf("%d, %d, %f\n", i, j, data[i][j]);
-        }
-    }
-
     printf("\n");
     for (i = 0; i < opt.n_points; ++i) {
         for(j = i+1; j < opt.n_points; j++){
-            printf("%d, %d, %f\n", i, j, l2_distance(data[i], data[j], opt.dimensions));
+            printf("%d, %d, %f\n", i, j, l2_distance(data[i], data[j], opt));
         }
     }
+
+    int idx;
+    double dist;
+    printf("\n");
+    for (i = 0; i < opt.n_points; ++i) {
+        find_nearest_centroid(data[i], data, opt, &idx, &dist);
+        printf("%d, %d, %f\n", i, idx, dist); 
+    }
+
 
     free(_data);
     free(data); 
@@ -37,16 +44,29 @@ int main(int argc, char **argv) {
 }
 
 
-double l2_distance(double *x1, double *x2, int d) {
+double l2_distance(double *x1, double *x2, options opt) {
     int i;
     double dist = 0.0;
-    for (i = 0; i < d; ++i) {
-        dist += (x1[i] - x2[i]) * (x1[i] - x2[i]); 
+    for (i = 0; i < opt.dimensions; ++i) {
+        dist += SQR(x1[i] - x2[i]); 
     }
     return sqrt(dist);
 }
 
-
+void find_nearest_centroid(double *x, double **centroids, options opt, \
+                            int *idx, double *distance) {
+    int i;
+    double curr_distance;
+    *distance = l2_distance(x, centroids[0], opt);
+    *idx = 0;
+    for(i = 1; i < opt.n_points; i++){
+        curr_distance = l2_distance(x, centroids[i], opt);
+        if( curr_distance < *distance) {
+            *distance = curr_distance;
+            *idx = i;
+        }
+    }
+}
 
 
 
