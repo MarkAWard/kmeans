07c9fe8e32198be66f0bb144ae980137e471532d
Mark Ward <markmaw@gmail.com>
2015-05-10 18:19:14 -0400

    debugging kmeans

--- seq-kmeans.c
+++ seq-kmeans.c
@@ -12,13 +12,13 @@ double l2_distance(double *x1, double *x2, options opt);
 void find_nearest_centroid(double *x, double **centroids, options opt, \
                             int *idx, double *distance);
 void kmeans(double **data, double **centroids, int *membership, \
-            double **new_centers, int *count_centers, options opt); 
+            double *inertia, options opt) ; 
 
 
 int main(int argc, char **argv) {
 
     srand(time(NULL));
-    int i, j;
+    int i;
     options opt;
 
     parse_command_line(argc, argv, &opt);
@@ -50,42 +50,27 @@ int main(int argc, char **argv) {
     int *membership = (int*) calloc(opt.n_points, sizeof(int));
     check(membership);
 
+    // int idx;
+    // double dist;
+    // printf("\n");
+    // for (i = 0; i < opt.n_points; ++i) {
+    //     find_nearest_centroid(data[i], centroids, opt, &idx, &dist);
+    //     printf("%d, %d, %f\n", i, idx, dist); 
+    // }
 
-    /**
-    *   These can be moved within the kmeans function
-    **/
-    
-    // allocate for new centroids that will be computed
-    double **new_centers = (double**) malloc(opt.n_centroids * sizeof(double*));
-    check(new_centers);
-    // and initialize to 0
-    double *_new_centers = (double*) calloc(opt.n_centroids * opt.dimensions, sizeof(double));
-    check(_new_centers);
-    for(i = 0; i < opt.n_centroids; i++) {
-        new_centers[i] = _new_centers + (i * opt.dimensions);
-    }
+    // print_vecs(centroids, opt, "centroids");
 
-    // allocate array to count points in each cluster, initialize to 0
-    int *count_centers = (int*) calloc(opt.n_centroids, sizeof(int));
+    double inertia = 0.0;
+    kmeans(data, centroids, membership, &inertia, opt);
 
-
-    int idx;
-    double dist;
-    printf("\n");
-    for (i = 0; i < opt.n_points; ++i) {
-        find_nearest_centroid(data[i], centroids, opt, &idx, &dist);
-        printf("%d, %d, %f\n", i, idx, dist); 
-    }
-
+    printf("\nINERTIA: %f\n", inertia);
+    print_vecs(centroids, opt, "centroids");
 
     free(_data);
     free(data); 
     free(centroids);
     free(_centroids);
     free(membership);
-    free(new_centers);
-    free(_new_centers);
-    free(count_centers);
 
     return 0;
 }
@@ -97,7 +82,6 @@ void initialize(double **data, double **centroids, options opt) {
     for(i = 0; i < opt.n_centroids; i++){
         while(In(idx = randint(opt.n_points), init, i));
         init[i] = idx;
-        printf("%d\n", idx);
         memcpy(centroids[i], data[idx], opt.dimensions * sizeof(double));
     }
     free(init);
@@ -129,15 +113,39 @@ void find_nearest_centroid(double *x, double **centroids, options opt, \
 
 
 void kmeans(double **data, double **centroids, int *membership, \
-            double **new_centers, int *count_centers, options opt) {
-    double inertia, dist, delta = 0.0;
+            double *inertia, options opt) {
+    
+    double dist, delta = (double) opt.n_points;
     int i, center, iters = 0;
+
+    // allocate for new centroids that will be computed
+    double **new_centers = (double**) malloc(opt.n_centroids * sizeof(double*));
+    check(new_centers);
+    // and initialize to 0
+    double *_new_centers = (double*) calloc(opt.n_centroids * opt.dimensions, sizeof(double));
+    check(_new_centers);
+    for(i = 0; i < opt.n_centroids; i++) {
+        new_centers[i] = _new_centers + (i * opt.dimensions);
+    }
+    // allocate array to count points in each cluster, initialize to 0
+    int *count_centers = (int*) calloc(opt.n_centroids, sizeof(int));
+
+    printf("\n");
+    printf("max_iter: %d\n", opt.max_iter);
+    printf("tol: %f\n", opt.tol);
+    
+    printf("\n");
+    printf("iters: %d\n", iters);
+    printf("delta: %f\n", delta);
+    printf("eps: %f\n", delta / ((double) opt.n_points));
+    print_vecs(centroids, opt, "centroids");
+
     while (delta / ((double) opt.n_points) > opt.tol && iters < opt.max_iter) {
         delta = 0.0;
-        inertia = 0.0;
+        *inertia = 0.0;
         for(i = 0; i < opt.n_points; i++){
             find_nearest_centroid(data[i], centroids, opt, &center, &dist);
-            inertia += dist;
+            *inertia += dist;
             if (membership[i] != center) {
                 delta++;
                 membership[i] = center;       
@@ -145,17 +153,44 @@ void kmeans(double **data, double **centroids, int *membership, \
             add(new_centers[center], data[i], opt);
             count_centers[center]++; 
         }
+        printf("\n");
         for(i = 0; i < opt.n_centroids; i++) {
             if(count_centers[i] == 0) {
-                // pick a random point
+                printf("HERE 0\n");
+                // pick a random point, not sure if this is the best option
+                add(new_centers[i], data[randint(opt.n_points)], opt);
+                // add to delta to ensure we dont stop after this
+                delta += opt.tol * opt.n_points + 1.0;
             }
             else {
+                printf("HERE 1: %d\n", count_centers[i]);
+                print_vec(new_centers[i], opt.dimensions);
                 // calculate the new center
+                div_by(new_centers[i], count_centers[i], opt);
+                print_vec(new_centers[i], opt.dimensions);
             }
         }
+        printf("\nBEFORE\n");
+        print_vecs(centroids, opt, "centroids");
+        memcpy(centroids, new_centers, opt.n_centroids * opt.dimensions * sizeof(double));
+        printf("AFTER\n");
+        print_vecs(centroids, opt, "centroids");
         // zero out new_centers and count_centers
-        iters++;
+        memset(new_centers,   0, opt.n_centroids * opt.dimensions * sizeof(double));
+        memset(count_centers, 0, opt.n_centroids * sizeof(int));
+        printf("AFTER MEMSET\n");
+        print_vecs(centroids, opt, "centroids");
+
+        iters++;        
+        printf("\n");
+        printf("iters: %d\n", iters);
+        printf("delta: %f\n", delta);
+        printf("eps: %f\n", delta / ((double) opt.n_points));
+        print_vecs(centroids, opt, "centroids");
     }
+    free(new_centers);
+    free(_new_centers);
+    free(count_centers);
 }
 
 
