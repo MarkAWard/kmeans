b8bdd4afdc61ff28f964c0d4a8d94d1c5655e521
Mark Ward <markmaw@gmail.com>
2015-05-09 23:07:45 -0400

    parse command line options

--- seq-kmeans.c
+++ seq-kmeans.c
@@ -3,33 +3,49 @@
 #include <math.h>
 #include "csvparser.h"
 
+#define DELIMITER ","
+
+void exit_with_help(){
+    printf("HELP HELP HELPPPP!!\n");
+    exit(1);
+}
+
+struct options {
+    char *filename;
+    char *sep;
+    int n_points;
+    int dimensions;
+};
+typedef struct options options;
+
+
+void parse_command_line(int argc, char **argv, options *opt);
 double l2_distance(double *x1, double *x2, int d);
 
 int main(int argc, char **argv) {
 
-    char *filename = argv[1];
-    int N = atoi(argv[2]);
-    int d = atoi(argv[3]);
+    options opt;
+    parse_command_line(argc, argv, &opt);
 
     int i, j;
-    double **data = (double**) malloc(N * sizeof(double*));
-    double *_data = (double*) malloc(N * d * sizeof(double));
-    for(j = 0; j < N; j++){
-        data[j] = _data + (j * d);
+    double **data = (double**) malloc(opt.n_points * sizeof(double*));
+    double *_data = (double*) malloc(opt.n_points * opt.dimensions * sizeof(double));
+    for(j = 0; j < opt.n_points; j++){
+        data[j] = _data + (j * opt.dimensions);
     }
 
-    read_data(filename, ",", data, N, d);
-    for(i=0; i<N; i++){
-        for(j=0; j<d; j++){
+    read_data(opt.filename, ",", data, opt.n_points, opt.dimensions);
+    for(i=0; i<opt.n_points; i++){
+        for(j=0; j<opt.dimensions; j++){
             printf("%d, %d, %f\n", i, j, data[i][j]);
         }
     }
 
     printf("\n");
-    for (i = 0; i < N; ++i) {
-    	for(j = i+1; j < N; j++){
-    		printf("%d, %d, %f\n", i, j, l2_distance(data[i], data[j], d));
-    	}
+    for (i = 0; i < opt.n_points; ++i) {
+        for(j = i+1; j < opt.n_points; j++){
+            printf("%d, %d, %f\n", i, j, l2_distance(data[i], data[j], opt.dimensions));
+        }
     }
 
     free(_data);
@@ -46,3 +62,36 @@ double l2_distance(double *x1, double *x2, int d) {
     }
     return sqrt(dist);
 }
+
+
+
+
+
+void parse_command_line(int argc, char **argv, options *opt) {
+    // default options
+    opt->sep = DELIMITER;
+    // parse options
+    int i;
+    for(i=1;i<argc;i++) {
+        printf("%d\n", i);
+        if(argv[i][0] != '-') break;
+        if(++i>=argc) exit_with_help();
+        switch(argv[i-1][1]) {
+            case 'f':
+                opt->filename = argv[i];
+                break;
+            case 'n':
+                opt->n_points = atoi(argv[i]);
+                break;
+            case 'd':
+                opt->dimensions = atoi(argv[i]);
+                break;
+            case 's':
+                opt->sep = argv[i];
+            default:
+                fprintf(stderr,"unknown option: -%c\n", argv[i-1][1]);
+                exit_with_help();
+        }
+    }
+}
+
