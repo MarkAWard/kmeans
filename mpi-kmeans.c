8696031edd7db6b3043750cf9856a7f22e7143cf
Mark Ward <markmaw@gmail.com>
2015-05-14 01:56:23 -0400

    kmeans single loop running

--- mpi-kmeans.c
+++ mpi-kmeans.c
@@ -2,13 +2,21 @@
 #include <stdlib.h>
 #include <time.h>
 #include <float.h> // DBL_MAX
+#include <math.h> // sqrt
 #include <string.h>
 #include <mpi.h>
 #include "helper.h"
 #include "mpifile.h"
 
+#define SQR(x) (x)*(x)
+
 void initialize(double **data, double **centroids, int *ppp, int rank, int size, options opt);
 int get_owner(int *point_id, int *ppp);
+double l2_distance(double *x1, double *x2, options opt);
+void find_nearest_centroid(double *x, double **centroids, options opt, \
+                            int *idx, double *distance);
+void _kmeans(double **data, double **centroids, int *membership, \
+            double *inertia, int rank, int size, int *ppp, options opt);
 
 int main( int argc, char **argv) {
 
@@ -48,23 +56,13 @@ int main( int argc, char **argv) {
 
   opt.n_points = total_rows;
   opt.dimensions = cols;
+  opt.local_rows = rows;
+
   if(mpi_rank == 0 && opt.verbose > 1) {
     printf("Total rows: %d\n", opt.n_points);
     for(r = 0; r < mpi_size; r++)
       printf("proc %d has %d\n", r, points_per_proc[r]);
   }
-  // allocate centroids, everyone gets their own copy
-  double **centroids = (double**) alloc2d(opt.n_centroids, opt.dimensions);
-  // allocate and initialize points' cluster memberships to 0
-  // only track the ones this process is responsible for
-  int *membership = (int*) calloc(rows, sizeof(int));
-  check(membership);
-
-  double inertia = DBL_MAX;
-
-  initialize(data, centroids, points_per_proc, mpi_rank, mpi_size, opt);
-
-
   for(r=0; r < mpi_size; r++) {
     MPI_Barrier(MPI_COMM_WORLD);
     if(mpi_rank == r && opt.verbose > 1) {
@@ -75,10 +73,28 @@ int main( int argc, char **argv) {
     }
   }
 
+  // allocate centroids, everyone gets their own copy
+  double **centroids = (double**) alloc2d(opt.n_centroids, opt.dimensions);
+  // allocate and initialize points' cluster memberships to 0
+  // only track the ones this process is responsible for
+  int *membership = (int*) calloc(rows, sizeof(int));
+  check(membership);
+
+  double inertia = DBL_MAX;
+
+  _kmeans(data, centroids, membership, &inertia, mpi_rank, mpi_size, points_per_proc, opt);
+
   if(mpi_rank == 0 && opt.verbose > 0) { 
-    printf("\nINERTIA: %f\n", inertia);
+    printf("\nINERTIA: %e\n", inertia);
     print_vecs(centroids, opt, "centroids");
   }
+
+  int center;
+  double distance;
+  for(i = 0; i < rows; i++){
+    find_nearest_centroid(data[i], centroids, opt, &center, &distance);
+    printf("proc %d: %d --- %d %f\n", mpi_rank, i, center, distance);
+  }  
 
 
   MPI_File_close(&filename);
@@ -145,7 +161,160 @@ int get_owner(int *point_id, int *ppp) {
   return i;
 }
 
+// unchanged from sequential code
+double l2_distance(double *x1, double *x2, options opt) {
+    int i;
+    double dist = 0.0;
+    for (i = 0; i < opt.dimensions; i++) {
+        dist += SQR(x1[i] - x2[i]); 
+    }
+    return sqrt(dist);
+}
 
+// unchanged from sequential code
+void find_nearest_centroid(double *x, double **centroids, options opt, \
+                            int *idx, double *distance) {
+    int i;
+    double curr_distance;
+    *distance = l2_distance(x, centroids[0], opt);
+    *idx = 0;
+    for(i = 1; i < opt.n_centroids; i++){
+        curr_distance = l2_distance(x, centroids[i], opt);
+        if( curr_distance < *distance) {
+            *distance = curr_distance;
+            *idx = i;
+        }
+    }
+}
+
+
+
+void _kmeans(double **data, double **centroids, int *membership, \
+            double *inertia, int rank, int size, int *ppp, options opt) {
+
+  MPI_Status status;
+
+    double dist, total_inertia, total_delta, delta = (double) opt.n_points;
+  int i, center, iters = 0;
+
+  // allocate for new centroids that will be computed
+  double **new_centers = (double**) alloc2d(opt.n_centroids, opt.dimensions);
+  memset(*new_centers, 0, opt.n_centroids * opt.dimensions * sizeof(double));
+
+  // allocate array to count points in each cluster, initialize to 0
+  int *count_centers = (int*) calloc(opt.n_centroids, sizeof(int));
+  check(count_centers);
+  int *new_count_centers = (int*) calloc(opt.n_centroids, sizeof(int));
+  check(new_count_centers);
+
+  // if a cluster has 0 points assigned use this for random reinitialization
+  double *point = (double*) malloc(opt.dimensions * sizeof(double));
+  check(point);
+  double *tofree = point;
+
+  initialize(data, centroids, ppp, rank, size, opt);
+  MPI_Bcast(*centroids, opt.n_centroids*opt.dimensions, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    
+  while (delta / ((double) opt.n_points) > opt.tol && iters < opt.max_iter) {
+    MPI_Barrier(MPI_COMM_WORLD);
+    delta = 0.0;
+    *inertia = 0.0;
+    for(i = 0; i < opt.local_rows; i++){
+        find_nearest_centroid(data[i], centroids, opt, &center, &dist);
+        *inertia += dist;
+        if (membership[i] != center) {
+            delta++;
+            membership[i] = center;       
+        }
+        add(new_centers[center], data[i], opt);
+        new_count_centers[center]++; 
+    }
+
+    MPI_Allreduce(*new_centers, *centroids, opt.n_centroids * opt.dimensions, 
+                  MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(new_count_centers, count_centers, opt.n_centroids, 
+                  MPI_INT, MPI_SUM, MPI_COMM_WORLD);
+
+    for(i = 0; i < opt.n_centroids; i++) {
+        if(count_centers[i] == 0) {
+          // process 0 is in charge of getting the new point
+          if(rank == 0) {
+            // printf("\nSHIT HERE\n");
+            int idx = randint(opt.n_points);
+            int owner = get_owner(&idx, ppp); 
+            if(owner != 0) {
+              // get the point from the other process
+              // printf("SHIT before\n");
+              MPI_Send(&idx, 1, MPI_INT, owner, 999, MPI_COMM_WORLD);
+              MPI_Recv(point, opt.dimensions, MPI_DOUBLE, owner, 999, MPI_COMM_WORLD, &status);
+              // printf("SHIT after\n");
+            } 
+            else {
+              // I own the point dont ask anyone else
+              point = data[idx];
+            }
+            memcpy(centroids[i], point, opt.dimensions * sizeof(double));
+            point = tofree;
+            // let the other processors know we are done
+            idx = -1;
+            for(i = 1; i < size; i++)
+              MPI_Send(&idx, 1, MPI_INT, i, 999, MPI_COMM_WORLD);
+            // printf("SHIT sent -1\n");
+          }
+          // other processes send point if needed
+          else {
+            int get_point;
+            while(1) {
+              MPI_Recv(&get_point, 1, MPI_INT, 0, 999, MPI_COMM_WORLD, &status);
+              if(get_point != -1) {
+                // printf("SHIT %d is sending %d\n", rank, get_point);
+                MPI_Send(data[get_point], opt.dimensions, MPI_DOUBLE, 0, 999, MPI_COMM_WORLD);
+              }
+              else {
+                // printf("SHIT get here %d\n", rank);
+                break;
+             }
+            }
+          }
+          // printf("SHIT we all done\n");
+          // broadcast this new point to everyone
+          MPI_Bcast(centroids[i], opt.dimensions, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+          // printf("SHIT broadcasted\n");
+          // add to delta to ensure we dont stop after this
+          delta += opt.tol * opt.local_rows + 1.0;
+        }
+        // all good to divide, count is not 0
+        else {
+          // calculate the new center
+          div_by(centroids[i], count_centers[i], opt);
+        }
+    }
+
+    // sum up the number of cluster assignments that changed
+    MPI_Allreduce(&delta, &total_delta, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    delta = total_delta;
+    // sum up the inertias
+    MPI_Allreduce(inertia, &total_inertia, 1, MPI_DOUBLE, MPI_SUM, MPI_COMM_WORLD);
+    *inertia = total_inertia;
+    // zero out new_centers and count_centers
+    memset(*new_centers, 0, opt.n_centroids * opt.dimensions * sizeof(double));
+    memset(new_count_centers, 0, opt.n_centroids * sizeof(int));
+    memset(count_centers, 0, opt.n_centroids * sizeof(int));
+
+    iters++;
+    if(opt.verbose > 1 && rank == 0) {
+        printf("\n\titers: %d\n", iters);
+        printf("\tdelta: %d\n", (int)   delta);
+        printf("\teps: %f\n", delta / ((double) opt.n_points));
+        printf("\tinertia: %f\n", *inertia);
+    }
+  }
+  free(*new_centers);
+  free(new_centers);
+  free(count_centers);
+  free(new_count_centers);
+  free(tofree);
+} 
 
 
 
