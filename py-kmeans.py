b79500d38985a3e459023c8c961b7e4311187756
Mark Ward <markmaw@gmail.com>
2015-05-16 01:17:42 -0400

    run from command line

--- py-kmeans.py
+++ py-kmeans.py
@@ -69,8 +69,38 @@ def kmeans(data, n_clusters=10, init='random', tol=0.0001, max_iter=300, best_of
 		loops += 1
 	return best_inertia, best_centroids, best_labels
 
-from sklearn.datasets import make_blobs
-data, y = make_blobs(n_features=100, centers=5, n_samples=10000)
-inertia, centers, labels = kmeans(data, best_of=1, max_iter=300, n_clusters=5, tol=0.0001)
 
+if __name__ == "__main__":
+	import argparse
 
+	parser = argparse.ArgumentParser(description="K-means algorithm")
+	parser.add_argument('-f', dest='filename', type=str, 
+						help="file containing data to be clustered")
+	parser.add_argument('-k', dest='n_centroids', type=int, default=5,
+						help="the number of clusters to find")
+	parser.add_argument('-s', dest='sep', type=str, default=',',
+						help="file delimiter")
+	parser.add_argument('-n', dest='n_points', type=int, default=-1,
+						help="number of points in file to read (optional)")
+	parser.add_argument('-d', dest='dimensions', type=int, default=-1,
+						help="fnumber of dimensions of input data (optional)")
+	parser.add_argument('-e', dest='tol', type=float, default=1e-5,
+						help="minimum fraction of points that don't change clusters to end kmeans loop")
+	parser.add_argument('-i', dest='max_iter', type=int, default=100,
+						help="maximum number of iterations within kmeans")
+	parser.add_argument('-t', dest='trials', type=int, default=25,
+						help="umber of kmeans trials to perform to find best clustering")
+	parser.add_argument('-v', dest='verbose', type=int, default=0,
+						help="control amount of printing 0, 1, 2 ")
+
+	args = parser.parse_args()
+
+	data = np.genfromtxt(args.filename, delimiter=args.sep)
+	if args.n_points != -1:
+		data = data[:args.n_points,:]
+	if args.dimensions != -1:
+		data = data[:,:args.dimensions]
+
+	inertia, centers, labels = kmeans(data, best_of=args.trials, max_iter=args.max_iter, 
+									n_clusters=args.n_centroids, tol=args.tol)
+	print inertia
