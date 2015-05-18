21b7d80fafb8184b2af9ac349bf13391742878f8
Mark Ward <markmaw@gmail.com>
2015-05-18 14:02:45 -0400

    smaller data, increasing number of centroids

---
+++ time_seq1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./seq-kmeans -f blobs_10000_10_k20.csv -k 5  -t 500 
+./seq-kmeans -f blobs_10000_10_k20.csv -k 10 -t 500 
+./seq-kmeans -f blobs_10000_10_k20.csv -k 15 -t 500 
+./seq-kmeans -f blobs_10000_10_k20.csv -k 20 -t 500 
+./seq-kmeans -f blobs_10000_10_k20.csv -k 25 -t 500 
+./seq-kmeans -f blobs_10000_10_k20.csv -k 30 -t 500 
+./seq-kmeans -f blobs_10000_10_k20.csv -k 35 -t 500 
+./seq-kmeans -f blobs_10000_10_k20.csv -k 40 -t 500 
+./seq-kmeans -f blobs_10000_10_k20.csv -k 45 -t 500 
+./seq-kmeans -f blobs_10000_10_k20.csv -k 50 -t 500 
+
