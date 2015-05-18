d352d053deca4e603fe0a835f714dda53501f20a
Mark Ward <markmaw@gmail.com>
2015-05-18 14:12:39 -0400

    bigger data, increase number of centroids

---
+++ time_seq2.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+./seq-kmeans -f otto_group_61878_93.csv -k 5  -t 500 
+./seq-kmeans -f otto_group_61878_93.csv -k 10 -t 500 
+./seq-kmeans -f otto_group_61878_93.csv -k 15 -t 500 
+./seq-kmeans -f otto_group_61878_93.csv -k 20 -t 500 
+./seq-kmeans -f otto_group_61878_93.csv -k 25 -t 500 
+./seq-kmeans -f otto_group_61878_93.csv -k 30 -t 500 
+./seq-kmeans -f otto_group_61878_93.csv -k 35 -t 500 
+./seq-kmeans -f otto_group_61878_93.csv -k 40 -t 500 
+./seq-kmeans -f otto_group_61878_93.csv -k 45 -t 500 
+./seq-kmeans -f otto_group_61878_93.csv -k 50 -t 500 
+
